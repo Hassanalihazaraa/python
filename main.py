d48a31d8d74e92f3bf2338eb782d8afcf944e0e9
Hassan <hassanalihazaraa@gmail.com>
2020-11-14 13:28:48 +0100

    looped through all seven days and displayed period-names, description and temperture

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from bs4 import BeautifulSoup
 import requests
 
@@ -6,4 +7,16 @@ soup = BeautifulSoup(page.content, 'html.parser')
 week = soup.find(id="seven-day-forecast-body")
 
 items = week.find_all(class_="tombstone-container")
-print(items)
+
+period_names = [item.find(class_="period-name").get_text() for item in items]
+short_desc = [item.find(class_="short-desc").get_text() for item in items]
+temp = [item.find(class_="temp").get_text() for item in items]
+
+weather_list = pd.DataFrame(
+    {
+        'period': period_names,
+        'short_description': short_desc,
+        'temperature': temp,
+    }
+)
+print(weather_list)
