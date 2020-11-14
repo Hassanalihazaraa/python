1d96c2b61d9324cbb391e6b7423242fe4e139f07
Hassan <hassanalihazaraa@gmail.com>
2020-11-14 13:13:37 +0100

    week and days

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from bs4 import BeautifulSoup
 import requests
 
-page = requests.get('https://openweathermap.org/city/2803138')
-soup = BeautifulSoup()
+page = requests.get('https://forecast.weather.gov/MapClick.php?lat=40.7146&lon=-74.0071')
+soup = BeautifulSoup(page.content, 'html.parser')
+week = soup.find(id="seven-day-forecast-body")
+
+items = week.find_all(class_="tombstone-container")
+print(items)
